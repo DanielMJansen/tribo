1da1491d78fd57cd7e2bf4912f5316f8b959b44f
DanielMJansen <danielodie@hotmail.com>
2017-04-05 12:28:53 +0000

    Update Produto.java

--- src/tribo/s/work/Produto.java
+++ src/tribo/s/work/Produto.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tribo.s.work;
+package tribo;
+import java.util.ArrayList;
 
-/**
- *
- * @author informatica
- */
 public class Produto {
 public String nome;
 public Float preco;
 public int codigo;
+String[] produtos = new String[300];
 }
