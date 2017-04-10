a06f42c7b8d1f739f9bcea2f8fdc03e3163b4a61
nathalia-tiedt <fernandanathalia88@gmail.com>
2017-04-10 12:25:00 +0000

    Update ControlaPermissao.java

--- src/tribo/s/work/ControlaPermissao.java
+++ src/tribo/s/work/ControlaPermissao.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tribo.s.work;
+package tribo;
 
-/**
- *
- * @author informatica
- */
 public interface ControlaPermissao {
         public boolean getPermissaoCadastra();
     public boolean getPermissaoRemove();
