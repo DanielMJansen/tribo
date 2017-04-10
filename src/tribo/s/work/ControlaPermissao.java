ff2ce995502127926415fd35c596be230386eb68
nathalia-tiedt <fernandanathalia88@gmail.com>
2017-04-10 12:25:26 +0000

    Update ControlaPermissao.java

--- src/tribo/s/work/ControlaPermissao.java
+++ src/tribo/s/work/ControlaPermissao.java
@@ -1,9 +1,19 @@
 package tribo;
-
+//Interface com controle de permissões do gerente e do funcionário.
 public interface ControlaPermissao {
-        public boolean getPermissaoCadastra();
+    //Permissão para cadastrar clientes com receitas
+    public boolean getPermissaoCadastraCliente();
+//Permissão de cadastrar produto
+    public boolean getPermissaoCadastra();
+//Permissão de remover funcionário do sistema/login
+    public boolean getPermissaoRemoveFuncionario();
+//Permissão para consultar preço do produto/remedio
+    public boolean getPermissaoConsultaPreco();
+//Permissão para remover produto/remédio
     public boolean getPermissaoRemove();
+//Permissão para vender produtos
     public boolean getPermissaoVenda();
+//Permissão para criar login de funcionário/gerente
     public boolean getPermissaoCriaLogin();
-    
+
 }
