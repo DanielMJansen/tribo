6e5bdf45d1f85b154f4743ab96e230c788e5c8e6
DanielMJansen <danielodie@hotmail.com>
2017-04-17 12:27:28 +0000

    Update Cliente.java

--- src/tribo/s/work/Cliente.java
+++ src/tribo/s/work/Cliente.java
@@ -1,5 +1,15 @@
 package tribo;
 
-public class Cliente {      
-}    
+import java.util.ArrayList;
 
+public class Cliente extends Pessoa {
+
+    public void CadastraCliente(ArrayList<Cliente> clientes) {
+        nome = Util.pedeStringNaoVazia("Qual o nome do cliente? (diferente de vazio).");
+        endereco = Util.pedeStringNaoVazia("Qual o endereço do cliente? (diferente de vazio).");
+        cpf = Util.pedeStringNaoVazia("Qual o CPF do cliente?");
+        telefone = Util.pedeStringNaoVazia("Qual o telefone do cliente?");
+        clientes.add(this);
+        System.out.println("Cliente cadastrada com sucesso.");
+    }
+}
