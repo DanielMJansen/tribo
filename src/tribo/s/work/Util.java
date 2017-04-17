63fba2bf20ac7e810aaada65c2d1a2a90d64b741
DanielMJansen <danielodie@hotmail.com>
2017-04-17 12:25:29 +0000

    Add files via upload

---
+++ src/tribo/s/work/Util.java
@@ -0,0 +1,26 @@
+package tribo;
+
+import java.util.Scanner;
+
+public class Util {
+
+    static String pedeStringNaoVazia(String mensagem) {
+        String entrada = "";
+        do {
+            System.out.println(mensagem);
+            entrada = new Scanner(System.in).nextLine().trim();
+        } while (entrada.isEmpty());
+        return entrada;
+    }
+
+    static float PedePrecoMaiorQueZero(String mensagem) {
+        float preco = 0;
+        do {
+            System.out.println(mensagem);
+            preco = new Scanner(System.in).nextFloat();
+        } while (preco <= 0);
+        return preco;
+    }
+    
+    
+}
