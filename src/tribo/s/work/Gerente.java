432bf38b772f461b64aaaf84bd2e4b1191499ea7
nathalia-tiedt <fernandanathalia88@gmail.com>
2017-04-19 12:23:13 +0000

    Update Gerente.java

--- src/tribo/s/work/Gerente.java
+++ src/tribo/s/work/Gerente.java
@@ -1,5 +1,8 @@
 package tribo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Gerente extends Funcionario {
 
     public Gerente(String login, String senha) {
@@ -40,4 +43,23 @@ public class Gerente extends Funcionario {
     public boolean getPermissaoCriaLogin() {
         return true;
     }
+
+    public void CadastraFuncionario(ArrayList<Funcionario> funcionarios) {
+        String leia;
+        do {
+            leia = Util.pedeStringNaoVazia("Você deseja criar o login de um funcionário simples(1) ou um gerente(2)?");
+        } while (!leia.equals("1") && !leia.equals("2"));
+        
+        String login = Util.pedeStringNaoVazia("Digite o login do funcionário/gerente");
+        String senha = Util.pedeStringNaoVazia("Digite a senha do funcionário/gerente");
+        
+        if (leia == "1") {
+            funcionarios.add(new Funcionario(login, senha));
+        } else if (leia == "2") {
+            funcionarios.add(new Gerente(login, senha));
+        } else {
+            System.out.println("Mensagem inserida incorreta, tente novamente.");
+        }
+    }
+
 }
